36da564fe5aa990c2a23525bc80e734ca432a42b
Hugo Marchadour <hugo.marchadour@obeo.fr>
2013-11-03 20:20:37 +0100

    Refactoring to avoid classloader exception.
    We use available eclipses bundles to load class instead of
    URIClassloader. It's clearly to enhance (handle osgi requirement).

--- plugins/org.obeonetwork.jar2uml/src/org/obeonetwork/jar2uml/internal/Jars2UML.java
+++ plugins/org.obeonetwork.jar2uml/src/org/obeonetwork/jar2uml/internal/Jars2UML.java
@@ -14,8 +14,12 @@ import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -27,11 +31,15 @@ import java.util.jar.JarFile;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipException;
 
+import org.eclipse.core.internal.runtime.InternalPlatform;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.osgi.util.ManifestElement;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleException;
 
 public class Jars2UML extends Job {
 
@@ -49,6 +57,22 @@ public class Jars2UML extends Job {
 
 	private final List<File> files;
 
+	private ClassLoader classLoader;
+
+	private Map<File, Set<Class<?>>> filesBinding;
+
+	private Map<String, Set<Class<?>>> classStore;
+	private Set<Class<?>> interfaces;
+	private Set<Class<?>> clazzes;
+	private Set<Class<?>> enums;
+	private Set<Class<?>> annotations;
+
+	private Map<String, Set<Class<?>>> externalStore;
+	private Set<Class<?>> extInterfaces;
+	private Set<Class<?>> extClazzes;
+	private Set<Class<?>> extEnums;
+	private Set<Class<?>> extAnnotations;
+
 	/**
 	 * 
 	 */
@@ -65,29 +89,73 @@ public class Jars2UML extends Job {
 	 */
 	@Override
 	protected IStatus run(IProgressMonitor monitor) {
-		Map<File, Map<String, Set<Class<?>>>> result = new HashMap<File, Map<String, Set<Class<?>>>>();
+
+		filesBinding = new HashMap<File, Set<Class<?>>>();
+
+		interfaces = new HashSet<Class<?>>();
+		clazzes = new HashSet<Class<?>>();
+		enums = new HashSet<Class<?>>();
+		annotations = new HashSet<Class<?>>();
+
+		classStore = new HashMap<String, Set<Class<?>>>();
+		classStore.put(INTERFACE_KEY, interfaces);
+		classStore.put(CLASS_KEY, clazzes);
+		classStore.put(ANNOTATION_KEY, annotations);
+		classStore.put(ENUM_KEY, enums);
+
+		extInterfaces = new HashSet<Class<?>>();
+		extClazzes = new HashSet<Class<?>>();
+		extEnums = new HashSet<Class<?>>();
+		extAnnotations = new HashSet<Class<?>>();
+
+		externalStore = new HashMap<String, Set<Class<?>>>();
+		externalStore.put(INTERFACE_KEY, extInterfaces);
+		externalStore.put(CLASS_KEY, extClazzes);
+		externalStore.put(ANNOTATION_KEY, extAnnotations);
+		externalStore.put(ENUM_KEY, extEnums);
+
 		monitor.beginTask("Handle selected jars", files.size());
+		List<URL> urls = new ArrayList<URL>();
+		for (File file : files) {
+			try {
+				urls.add(file.toURI().toURL());
+			} catch (MalformedURLException e) {
+				e.printStackTrace();
+			}
+		}
+		classLoader = new URLClassLoader(urls.toArray(new URL[0]), this.getClass().getClassLoader());
+
 		for (File file : files) {
 			if (monitor.isCanceled()) {
 				return Status.CANCEL_STATUS;
 			}
 			monitor.subTask("Handle " + file.getName());
 			try {
-				Map<String, Set<Class<?>>> foundItems = loadAndScanJar(file);
-				result.put(file, foundItems);
+				scanClassEntries(file);
 			} catch (ZipException e) {
+				System.err.println("catched");
 				e.printStackTrace();
 			} catch (IOException e) {
+				System.err.println("catched");
+				e.printStackTrace();
+			} catch (Exception e) {
+				System.err.println("catched");
 				e.printStackTrace();
 			}
 			monitor.worked(1);
 		}
-		if (result.keySet().size() > 1) {
-			UMLModelHelper.createModel(project, "jars2uml.uml", result);
+
+		for (File file : files) {
+			exploreRelations(file);
+		}
+
+		if (filesBinding.keySet().size() > 1) {
+			UMLModelHelper.createModel(project, "jars2uml.uml", filesBinding, classStore, externalStore);
 		} else {
-			if (result.keySet().size() == 1) {
-				File file = result.keySet().iterator().next();
-				UMLModelHelper.createModel(project, file.getName().replace(".jar", ".uml"), result);
+			if (filesBinding.keySet().size() == 1) {
+				File file = filesBinding.keySet().iterator().next();
+				UMLModelHelper.createModel(project, file.getName().replace(".jar", ".uml"), filesBinding, classStore,
+						externalStore);
 			}
 		}
 		return Status.OK_STATUS;
@@ -111,35 +179,29 @@ public class Jars2UML extends Job {
 		}
 	}
 
-	private Map<String, Set<Class<?>>> loadAndScanJar(File jarFile) throws ZipException, IOException {
-
-		Set<Class<?>> externals = new HashSet<Class<?>>();
-		Set<Class<?>> interfaces = new HashSet<Class<?>>();
-		Set<Class<?>> clazzes = new HashSet<Class<?>>();
-		Set<Class<?>> enums = new HashSet<Class<?>>();
-		Set<Class<?>> annotations = new HashSet<Class<?>>();
-
-		Map<String, Set<Class<?>>> classes = new HashMap<String, Set<Class<?>>>();
-		classes.put(EXTERNAL_KEY, externals);
-		classes.put(INTERFACE_KEY, interfaces);
-		classes.put(CLASS_KEY, clazzes);
-		classes.put(ANNOTATION_KEY, annotations);
-		classes.put(ENUM_KEY, enums);
-
-		URL[] urls = new URL[] {jarFile.toURI().toURL()};
-		URLClassLoader classLoader = new URLClassLoader(urls, this.getClass().getClassLoader());
+	private void scanClassEntries(File jarFile) throws ZipException, IOException {
 
 		// Your jar file
 		JarFile jar = new JarFile(jarFile);
 		// Getting the files into the jar
 		Enumeration<? extends JarEntry> enumeration = jar.entries();
 
+		Set<Class<?>> clazzes = new HashSet<Class<?>>();
+
 		// Iterates into the files in the jar file
 		while (enumeration.hasMoreElements()) {
 			ZipEntry zipEntry = enumeration.nextElement();
 
-			// Is this a class?
-			if (zipEntry.getName().endsWith(".class")) {
+			if (zipEntry.getName().endsWith(".MF")) {
+				try {
+					Map manifest = ManifestElement.parseBundleManifest(jar.getInputStream(zipEntry), null);
+					String requiredBundles = (String) manifest.get("Require-Bundle");
+					// TODO: handle requiredBundles
+				} catch (BundleException e) {
+					e.printStackTrace();
+				}
+			} else if (zipEntry.getName().endsWith(".class")) {
+				// Is this a class?
 
 				// Relative path of file into the jar.
 				String className = zipEntry.getName();
@@ -148,66 +210,136 @@ public class Jars2UML extends Job {
 				className = className.replace(".class", "").replace("/", ".");
 				if (!className.contains("$")) {
 					// Load class definition from JVM
-					try {
-						Class<?> clazz = classLoader.loadClass(className);
-						try {
-							addInMap(clazz, classes);
-
-							// Supertype
-							Class<?> superclass = clazz.getSuperclass();
-							if (superclass != null) {
-								addInMap(superclass, classes);
-							}
-							Class<?>[] implementedInterfaces = clazz.getInterfaces();
-							for (Class<?> implementedInterface : implementedInterfaces) {
-								addInMap(implementedInterface, classes);
-							}
-
-							// Attributes
-							Field[] fields = clazz.getFields();
-							for (Field field : fields) {
-								Class<?> fieldClazz = field.getType();
-								addInMap(fieldClazz, classes);
-							}
-							// Methods
-							try {
-								Method[] methods = clazz.getMethods();
-								for (Method method : methods) {
-									Class<?> returnType = method.getReturnType();
-									addInMap(returnType, classes);
-									Class<?>[] parameterTypes = method.getParameterTypes();
-									for (Class<?> parameterType : parameterTypes) {
-										addInMap(parameterType, classes);
-									}
-
-								}
-							} catch (NoClassDefFoundError e) {
-								System.err.println(e.getMessage());
-							}
-						} catch (ClassCastException e) {
-							System.err.println(e.getMessage());
-						}
-					} catch (ClassNotFoundException e) {
-						System.err.println(e.getMessage());
-					} catch (Exception e) {
-						System.err.println(e.getMessage());
+					Class<?> clazz = resolveClassWithBundles(className);
+					if (clazz != null) {
+						clazzes.add(clazz);
+						addInMap(clazz, classStore);
 					}
 				}
 			}
 		}
+		filesBinding.put(jarFile, clazzes);
 		jar.close();
-		return classes;
 	}
 
-	private class ClassProxy {
+	private void exploreRelations(File jarFile) {
 
-		String name;
+		for (Class<?> clazz : filesBinding.get(jarFile)) {
 
-		String packageName;
+			// Supertype
+			Class<?> superclass = findSuperclass(clazz);
+			if (superclass != null && isExternal(clazz)) {
+				addInMap(superclass, externalStore);
+			}
+			for (Class<?> implementedInterface : findInterfaces(clazz)) {
+				if (isExternal(implementedInterface)) {
+					addInMap(implementedInterface, externalStore);
+				}
+			}
 
-		public ClassProxy(String name, String packageName) {
-			this.name = name;
-			this.packageName = packageName;
+			// Attributes
+			for (Field field : findAttributes(clazz)) {
+				if (isExternal(field.getType())) {
+					addInMap(field.getType(), externalStore);
+				}
+			}
+			// Methods
+			for (Method method : findMethodes(clazz)) {
+				Class<?> returnType = findMethodReturn(method);
+				if (returnType != null && isExternal(returnType)) {
+					addInMap(returnType, externalStore);
+				}
+				for (Class<?> parameterType : findMethodeParams(method)) {
+					if (isExternal(parameterType)) {
+						addInMap(parameterType, externalStore);
+					}
+				}
+			}
 		}
+	}
+
+	private Class<?> findSuperclass(Class<?> clazz) {
+		try {
+			return clazz.getSuperclass();
+		} catch (NoClassDefFoundError e) {
+			System.err.println("NoClassDefFoundError:findSuperclass " + e.getMessage());
+			return null;
+		}
+	}
+
+	private List<Class<?>> findInterfaces(Class<?> clazz) {
+		try {
+			return Arrays.asList(clazz.getInterfaces());
+		} catch (NoClassDefFoundError e) {
+			System.err.println("NoClassDefFoundError:findInterfaces " + e.getMessage());
+			return Collections.EMPTY_LIST;
+		}
+	}
+
+	private List<Field> findAttributes(Class<?> clazz) {
+		try {
+			return Arrays.asList(clazz.getFields());
+		} catch (NoClassDefFoundError e) {
+			System.err.println("NoClassDefFoundError:findAttributes " + e.getMessage());
+			return Collections.EMPTY_LIST;
+		}
+	}
+
+	private List<Method> findMethodes(Class<?> clazz) {
+		try {
+			return Arrays.asList(clazz.getMethods());
+		} catch (NoClassDefFoundError e) {
+			System.err.println("NoClassDefFoundError:findMethodes " + e.getMessage());
+			return Collections.EMPTY_LIST;
+		}
+	}
+
+	private Class<?> findMethodReturn(Method method) {
+		try {
+			return method.getReturnType();
+		} catch (NoClassDefFoundError e) {
+			System.err.println("NoClassDefFoundError:findMethodReturn " + e.getMessage());
+			return null;
+		}
+	}
+
+	private List<Class<?>> findMethodeParams(Method method) {
+		try {
+			return Arrays.asList(method.getParameterTypes());
+		} catch (NoClassDefFoundError e) {
+			System.err.println("NoClassDefFoundError:findMethodeParams " + e.getMessage());
+			return Collections.EMPTY_LIST;
+		}
+	}
+
+	private Class<?> resolveClass(String className) {
+		try {
+			Class<?> loadClass = classLoader.loadClass(className);
+			return loadClass;
+		} catch (ClassNotFoundException e1) {
+			return resolveClassWithBundles(className);
+		} catch (NoClassDefFoundError e1) {
+			return resolveClassWithBundles(className);
+		}
+	}
+
+	private boolean isExternal(Class<?> clazz) {
+		return !(interfaces.contains(clazz) || clazzes.contains(clazz) || enums.contains(clazz) || annotations
+				.contains(clazz));
+	}
+
+	private Class<?> resolveClassWithBundles(String className) {
+		// TODO: enhance the class resolver
+		Bundle[] bundles = InternalPlatform.getDefault().getBundleContext().getBundles();
+		for (Bundle bundle : bundles) {
+			try {
+				Class<?> loadClass = bundle.loadClass(className);
+				return loadClass;
+			} catch (ClassNotFoundException e) {
+				// try another bundle
+			}
+		}
+		System.err.println("Could not resolve " + className);
+		return null;
 	}
 }
--- plugins/org.obeonetwork.jar2uml/src/org/obeonetwork/jar2uml/internal/UMLModelHelper.java
+++ plugins/org.obeonetwork.jar2uml/src/org/obeonetwork/jar2uml/internal/UMLModelHelper.java
@@ -59,25 +59,48 @@ public final class UMLModelHelper {
 	 * @param map
 	 *            The map of file/items (Classes, interfaces...)
 	 */
-	public static void createModel(IProject project, String modelName,
-			Map<File, Map<String, Set<Class<?>>>> map) {
+	public static void createModel(IProject project, String modelName, Map<File, Set<Class<?>>> filesBinding,
+			Map<String, Set<Class<?>>> classStore, Map<String, Set<Class<?>>> externalStore) {
 
-		final URI semanticModelURI = URI.createPlatformResourceURI('/' + project.getName() + '/' + modelName,
-				true);
+		final URI semanticModelURI = URI.createPlatformResourceURI('/' + project.getName() + '/' + modelName, true);
 		Resource res = new ResourceSetImpl().createResource(semanticModelURI);
 		final Model model = UMLFactory.eINSTANCE.createModel();
 		res.getContents().add(model);
 		model.setName(modelName.replace(".uml", ""));
 		importPrimitiveTypes(model, UMLResource.JAVA_PRIMITIVE_TYPES_LIBRARY_URI);
 
-		for (File file : map.keySet()) {
+		final Map<Element, Class<?>> indexUML2Java = new HashMap<Element, Class<?>>();
+		final Map<Class<?>, Element> indexJava2UML = new HashMap<Class<?>, Element>();
+
+		if (!externalStore.isEmpty()) {
+			Component createdComponent = UMLFactory.eINSTANCE.createComponent();
+			model.getPackagedElements().add(createdComponent);
+			createdComponent.setName("external");
+			for (String key : externalStore.keySet()) {
+				Map<Map<Element, Class<?>>, Map<Class<?>, Element>> externalStoreIndex = createModelElements(
+						createdComponent, externalStore.get(key));
+				Set<Entry<Map<Element, Class<?>>, Map<Class<?>, Element>>> entrySet = externalStoreIndex.entrySet();
+				for (Entry<Map<Element, Class<?>>, Map<Class<?>, Element>> entry : entrySet) {
+					indexUML2Java.putAll(entry.getKey());
+					indexJava2UML.putAll(entry.getValue());
+				}
+			}
+		}
+
+		for (File file : filesBinding.keySet()) {
 			Component createdComponent = UMLFactory.eINSTANCE.createComponent();
 			model.getPackagedElements().add(createdComponent);
 			createdComponent.setName(file.getName());
-			Map<Map<Element, Class<?>>, Map<Class<?>, Element>> index = createElements(createdComponent,
-					map.get(file));
-			handleElementRelations(model, index);
+			Map<Map<Element, Class<?>>, Map<Class<?>, Element>> storeIndex = createModelElements(createdComponent,
+					filesBinding.get(file));
+
+			Set<Entry<Map<Element, Class<?>>, Map<Class<?>, Element>>> entrySet = storeIndex.entrySet();
+			for (Entry<Map<Element, Class<?>>, Map<Class<?>, Element>> entry : entrySet) {
+				indexUML2Java.putAll(entry.getKey());
+				indexJava2UML.putAll(entry.getValue());
+			}
 		}
+		handleElementRelations(model, indexUML2Java, indexJava2UML);
 		try {
 			res.save(Maps.newHashMap());
 		} catch (IOException e) {
@@ -85,156 +108,143 @@ public final class UMLModelHelper {
 		}
 	}
 
-	private static void handleElementRelations(Model model,
-			Map<Map<Element, Class<?>>, Map<Class<?>, Element>> index) {
+	private static void handleElementRelations(Model model, Map<Element, Class<?>> indexUML2Java,
+			Map<Class<?>, Element> indexJava2UML) {
 
-		Set<Entry<Map<Element, Class<?>>, Map<Class<?>, Element>>> entrySet = index.entrySet();
-		for (Entry<Map<Element, Class<?>>, Map<Class<?>, Element>> entry : entrySet) {
-			Map<Element, Class<?>> indexUML2Java = entry.getKey();
-			Map<Class<?>, Element> indexJava2UML = entry.getValue();
+		for (Element element : indexUML2Java.keySet()) {
+			if (element instanceof Interface) {
+				Class<?> javaInterface = indexUML2Java.get(element);
+				Interface anInterface = (Interface) element;
+				// TODO : handle Interface
+			} else if (element instanceof org.eclipse.uml2.uml.Class) {
+				Class<?> javaClass = indexUML2Java.get(element);
+				org.eclipse.uml2.uml.Class aClass = (org.eclipse.uml2.uml.Class) element;
 
-			for (Element element : indexUML2Java.keySet()) {
-				if (element instanceof Interface) {
-					Class<?> javaInterface = indexUML2Java.get(element);
-					Interface anInterface = (Interface)element;
-					// TODO : handle Interface
-				} else if (element instanceof org.eclipse.uml2.uml.Class) {
-					Class<?> javaClass = indexUML2Java.get(element);
-					org.eclipse.uml2.uml.Class aClass = (org.eclipse.uml2.uml.Class)element;
-
-					// Methodes
-					try {
-						for (Method method : javaClass.getMethods()) {
-							Operation createOwnedOperation = aClass.createOwnedOperation(method.getName(),
-									null, null);
-							Element returnType = indexJava2UML.get(method.getReturnType());
-							if (returnType != null) {
-								createOwnedOperation.createReturnResult("return", (Type)returnType);
-							} else {
-								List<Package> importedPackages = model.getImportedPackages();
-								for (Package package1 : importedPackages) {
-									NamedElement member = package1
-											.getMember(method.getReturnType().getName());
-									if (member instanceof Type) {
-										returnType = (Type)member;
-									}
-								}
-								if (returnType != null) {
-									createOwnedOperation.createReturnResult("return", (Type)returnType);
-								} else {
-									System.out.println("not find in the model "
-											+ method.getReturnType().getName());
-								}
-							}
-							Class<?>[] parameterTypes = method.getParameterTypes();
-							for (Class<?> parameterType : parameterTypes) {
-								Element paramType = indexJava2UML.get(parameterType);
-								if (paramType != null) {
-									createOwnedOperation.createOwnedParameter("", (Type)paramType);
-								} else {
-									List<Package> importedPackages = model.getImportedPackages();
-									for (Package package1 : importedPackages) {
-										NamedElement member = package1.getMember(parameterType.getName());
-										if (member instanceof Type) {
-											paramType = (Type)member;
-										}
-									}
-									if (paramType != null) {
-										createOwnedOperation.createOwnedParameter("", (Type)paramType);
-									} else {
-										System.out.println("not find in the model "
-												+ method.getReturnType().getName());
-									}
-								}
+				// Methods
+				for (Method method : javaClass.getMethods()) {
+					Operation createOwnedOperation = aClass.createOwnedOperation(method.getName(), null, null);
+					Element returnType = indexJava2UML.get(method.getReturnType());
+					if (returnType != null) {
+						createOwnedOperation.createReturnResult("return", (Type) returnType);
+					} else {
+						List<Package> importedPackages = model.getImportedPackages();
+						for (Package package1 : importedPackages) {
+							NamedElement member = package1.getMember(method.getReturnType().getName());
+							if (member instanceof Type) {
+								returnType = member;
 							}
 						}
-					} catch (NoClassDefFoundError e) {
-						System.err.println(e.getMessage());
+						if (returnType != null) {
+							createOwnedOperation.createReturnResult("return", (Type) returnType);
+						} else {
+							String name = method.getReturnType().getName();
+							if (!"void".equals(name)) {
+								System.out.println("not found in the model " + method.getReturnType().getName());
+							}
+						}
 					}
-
-					// Attributes
-					try {
-						for (Field field : javaClass.getFields()) {
-							Class<?> fieldType = field.getType();
-							if (indexJava2UML.containsKey(fieldType)) {
-								Element fieldUMLType = indexJava2UML.get(fieldType);
-								if (fieldUMLType instanceof Type) {
-									aClass.createOwnedAttribute(field.getName(), (Type)fieldUMLType);
-								} else {
-									System.err.println(fieldUMLType);
-								}
-							} else {
-								Class<?> currentType = field.getType();
-								int cardianality = 1;
-								if (currentType.isArray()) {
-									cardianality = -1;
-									currentType = currentType.getComponentType();
-								}
-
-								NamedElement member = model.getMember(currentType.getName());
+					Class<?>[] parameterTypes = method.getParameterTypes();
+					for (Class<?> parameterType : parameterTypes) {
+						Element paramType = indexJava2UML.get(parameterType);
+						if (paramType != null) {
+							createOwnedOperation.createOwnedParameter("", (Type) paramType);
+						} else {
+							List<Package> importedPackages = model.getImportedPackages();
+							for (Package package1 : importedPackages) {
+								NamedElement member = package1.getMember(parameterType.getName());
 								if (member instanceof Type) {
-									Type foundType = (Type)member;
-									aClass.createOwnedAttribute(field.getName(), foundType, 0, cardianality);
-								} else {
-									Class<? extends Class> class1 = field.getType().getClass();
-									System.err.println("not handled type " + field.getType().getName());
+									paramType = member;
+								}
+							}
+							if (paramType != null) {
+								createOwnedOperation.createOwnedParameter("", (Type) paramType);
+							} else {
+								if (!"void".equals(method.getReturnType().getName())) {
+									System.out.println("not found in the model " + method.getReturnType().getName());
 								}
 							}
 						}
-					} catch (NoClassDefFoundError e) {
-						System.err.println(e.getMessage());
 					}
-				} else if (element instanceof Enumeration) {
-					Class<?> javaEnumeration = indexUML2Java.get(element);
-					Enumeration anEnumeration = (Enumeration)element;
-					// TODO : handle Enumeration
 				}
+
+				// Attributes
+				for (Field field : javaClass.getFields()) {
+					Class<?> fieldType = field.getType();
+					if (indexJava2UML.containsKey(fieldType)) {
+						Element fieldUMLType = indexJava2UML.get(fieldType);
+						if (fieldUMLType instanceof Type) {
+							aClass.createOwnedAttribute(field.getName(), (Type) fieldUMLType);
+						} else {
+							System.err.println(fieldUMLType);
+						}
+					} else {
+						Class<?> currentType = field.getType();
+						int cardianality = 1;
+						if (currentType.isArray()) {
+							cardianality = -1;
+							currentType = currentType.getComponentType();
+						}
+
+						NamedElement member = model.getMember(currentType.getName());
+						if (member instanceof Type) {
+							Type foundType = (Type) member;
+							aClass.createOwnedAttribute(field.getName(), foundType, 0, cardianality);
+						} else {
+							Class<? extends Class> class1 = field.getType().getClass();
+							System.err.println("not handled type " + field.getType().getName());
+						}
+					}
+				}
+			} else if (element instanceof Enumeration) {
+				Class<?> javaEnumeration = indexUML2Java.get(element);
+				Enumeration anEnumeration = (Enumeration) element;
+				// TODO : handle Enumeration
 			}
 		}
 	}
 
-	private static Map<Map<Element, Class<?>>, Map<Class<?>, Element>> createElements(Component parent,
-			Map<String, Set<Class<?>>> foundItems) {
+	private static Map<Map<Element, Class<?>>, Map<Class<?>, Element>> createModelElements(Component parent,
+			Set<Class<?>> foundItems) {
 
 		final Map<Element, Class<?>> indexUML2Java = new HashMap<Element, Class<?>>();
 		final Map<Class<?>, Element> indexJava2UML = new HashMap<Class<?>, Element>();
 		final Map<Map<Element, Class<?>>, Map<Class<?>, Element>> index = new HashMap<Map<Element, Class<?>>, Map<Class<?>, Element>>();
 		index.put(indexUML2Java, indexJava2UML);
 
-		final Set<Class<?>> classes = foundItems.get(Jars2UML.CLASS_KEY);
+		for (Class<?> item : foundItems) {
 
-		for (Class<?> aClass : classes) {
-			if (aClass.getSimpleName() != null && aClass.getSimpleName().length() > 0) {
-				org.eclipse.uml2.uml.Package pack = handlePackage(parent, aClass.getPackage());
-				org.eclipse.uml2.uml.Class createClass = pack.createOwnedClass(aClass.getSimpleName(),
-						Modifier.isAbstract(aClass.getModifiers()));
-				indexUML2Java.put(createClass, aClass);
-				indexJava2UML.put(aClass, createClass);
-			}
-		}
-
-		final Set<Class<?>> interfaces = foundItems.get(Jars2UML.INTERFACE_KEY);
-
-		for (Class<?> anInterface : interfaces) {
-			if (anInterface.getSimpleName() != null && anInterface.getSimpleName().length() > 0) {
-				if (!anInterface.isPrimitive()) {
-					org.eclipse.uml2.uml.Package pack = handlePackage(parent, anInterface.getPackage());
-					Interface createInterface = pack.createOwnedInterface(anInterface.getSimpleName());
-					indexUML2Java.put(createInterface, anInterface);
-					indexJava2UML.put(anInterface, createInterface);
+			if (item.isInterface()) {
+				Class<?> anInterface = item;
+				if (anInterface.getSimpleName() != null && anInterface.getSimpleName().length() > 0) {
+					if (!anInterface.isPrimitive()) {
+						org.eclipse.uml2.uml.Package pack = handlePackage(parent, anInterface.getPackage());
+						Interface createInterface = pack.createOwnedInterface(anInterface.getSimpleName());
+						indexUML2Java.put(createInterface, anInterface);
+						indexJava2UML.put(anInterface, createInterface);
+					}
+				}
+			} else if (item.isAnnotation()) {
+				// TODO: handle annotation
+			} else if (item.isEnum()) {
+				Class<?> anEnum = item;
+				if (anEnum.getSimpleName() != null && anEnum.getSimpleName().length() > 0) {
+					org.eclipse.uml2.uml.Package pack = handlePackage(parent, anEnum.getPackage());
+					Enumeration createEnumeration = pack.createOwnedEnumeration(anEnum.getSimpleName());
+					indexUML2Java.put(createEnumeration, anEnum);
+					indexJava2UML.put(anEnum, createEnumeration);
+				}
+			} else {
+				Class<?> aClass = item;
+				if (aClass.getSimpleName() != null && aClass.getSimpleName().length() > 0) {
+					org.eclipse.uml2.uml.Package pack = handlePackage(parent, aClass.getPackage());
+					org.eclipse.uml2.uml.Class createClass = pack.createOwnedClass(aClass.getSimpleName(),
+							Modifier.isAbstract(aClass.getModifiers()));
+					indexUML2Java.put(createClass, aClass);
+					indexJava2UML.put(aClass, createClass);
 				}
 			}
 		}
 
-		final Set<Class<?>> enums = foundItems.get(Jars2UML.ENUM_KEY);
-		for (Class<?> anEnum : enums) {
-			if (anEnum.getSimpleName() != null && anEnum.getSimpleName().length() > 0) {
-				org.eclipse.uml2.uml.Package pack = handlePackage(parent, anEnum.getPackage());
-				Enumeration createEnumeration = pack.createOwnedEnumeration(anEnum.getSimpleName());
-				indexUML2Java.put(createEnumeration, anEnum);
-				indexJava2UML.put(anEnum, createEnumeration);
-			}
-		}
 		return index;
 	}
 
@@ -247,7 +257,7 @@ public final class UMLModelHelper {
 				for (PackageableElement packageableElement : packagedElements) {
 					if (packageableElement instanceof org.eclipse.uml2.uml.Package) {
 						if (subpackage.equals(packageableElement.getName())) {
-							current = (org.eclipse.uml2.uml.Package)packageableElement;
+							current = (org.eclipse.uml2.uml.Package) packageableElement;
 							break;
 						}
 					}
@@ -281,8 +291,7 @@ public final class UMLModelHelper {
 		final ResourceSet resourceSet = namespace.eResource().getResourceSet();
 		final Resource resource = resourceSet.getResource(URI.createURI(libraryUri), true);
 
-		final Package root = (Package)EcoreUtil.getObjectByType(resource.getContents(),
-				UMLPackage.Literals.PACKAGE);
+		final Package root = (Package) EcoreUtil.getObjectByType(resource.getContents(), UMLPackage.Literals.PACKAGE);
 		// We check if a package import already exists
 		if (!namespace.getImportedPackages().contains(root)) {
 			namespace.createPackageImport(root);
