31e96ffa48db188c47937635107bc1b1c5a1d821
Hugo Marchadour <hugo.marchadour@obeo.fr>
2013-11-16 21:10:42 +0100

    Provide a test plugin.

---
+++ plugins/org.obeonetwork.jar2uml.core.tests/src/org/obeonetwork/jar2uml/tests/unit/UMLModelHelperTest.java
@@ -0,0 +1,24 @@
+package org.obeonetwork.jar2uml.tests.unit;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class UMLModelHelperTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testCreateModel() {
+		fail("Not yet implemented");
+	}
+
+}
